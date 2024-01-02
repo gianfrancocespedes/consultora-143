a0bf3a7532bc51ddc1e845c9b79a793d2d03baba
Gianfranco Céspedes <gianmcf2@gmail.com>
2024-01-02 00:01:45 -0500

    Sidebar Menu

---
+++ src/scripts/sidebar.js
@@ -0,0 +1,13 @@
+export const sidebar = [
+    {
+        itemName: "Pages",
+        svgName: "pages.svg",
+        dropdown: "dropdown-pages",
+        children: [
+            {
+                childrenName: "Settings",
+                href: "/settings", 
+            }
+        ]
+    }
+];
